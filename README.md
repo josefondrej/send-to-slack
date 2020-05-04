fdb5e89d7dae2b394b3c5bdd3390b54af8ba2bf5
josef <josef.ondrej@outlook.com>
2020-05-04 11:40:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Send to Slack
+=============
+Set of utility functions for sending files and plots from python directly to slack.
+
+### Prerequisities
+1. You need to install requirements. From the `send-to-slack` directory run: 
+    ```
+    python -m pip install -r requirements.txt
+    ```
+1. You need to set the environemental variable `SLACK_BOT_TOKEN` to the token you obtain from https://api.slack.com. 
+The token should look like something like 
+    ```
+    xoxb-1234567891011-1213141516171-AbCDEFGhijklmnOPQRSTuVWx
+    ```
+1. Then you can import the library in your project and use it like 
+    ```python
+    from send_to_slack.send_file import send_file
+    
+    file_path = "C:/Temp/test_file.txt"
+    channel = "#general"
+    send_file(file_path=file_path, channel=channel)
+    
+    ```   
---
+++ send_to_slack/send_file.py
@@ -0,0 +1,78 @@
+import io
+import os
+from typing import Optional
+
+import requests
+
+_slack_bot_token_env = "SLACK_BOT_TOKEN"
+_file_api_endpoint = "https://slack.com/api/files.upload"
+
+try:
+    _bot_token = os.environ[_slack_bot_token_env]
+except:
+    raise ValueError(
+        f"You must set the environmental variable {_slack_bot_token_env} to your slack bot token which you can find at https://api.slack.com/.")
+
+
+def send_bytes(file: io.BufferedReader, channel: str, file_name: Optional[str] = None, comment: Optional[str] = None,
+               file_type: Optional[str] = None) -> requests.Response:
+    if file_name is None:
+        file_name = "Unnamed file."
+
+    headers = {
+        "Authorization": f"Bearer {_bot_token}",
+    }
+
+    files = {
+        "file": (file_name, file),
+        "initial_comment": (None, comment),
+        "channels": (None, channel),
+    }
+
+    if file_type is not None:
+        files["file_type"] = file_type
+
+    response = requests.post(_file_api_endpoint, headers=headers, files=files)
+    return response
+
+
+def send_file(file_path: str, channel: str, comment: Optional[str] = None) -> requests.Response:
+    file_name = file_path.split("/")[-1]
+    if comment is None:
+        comment = f"Sending file `{file_name}`."
+    with open(file_path, "rb") as file:
+        response = send_bytes(file=file, channel=channel, file_name=file_name, comment=comment)
+        return response
+
+
+def plot_to_slack(channel: str, plot_name: Optional[str] = None, comment: Optional[str] = None,
+                  format: str = "pdf") -> requests.Response:
+    if plot_name is None:
+        plot_name = "Unnamed figure."
+
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format=format)
+    buffer.seek(0)
+
+    response = send_bytes(file=buffer, channel=channel, file_name=plot_name, comment=comment)
+    return response
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    channel = "#general"  # alternatively use "@user.name"
+
+    # Test plot to slack
+    n_obs = 200
+    plt.scatter(np.random.randn(n_obs), np.random.randn(n_obs), alpha=0.5, color="deepskyblue")
+    plt.title("Scatterplot")
+    plt.xlabel("x-axis")
+    plt.ylabel("y-axis")
+    plt.grid()
+    response = plot_to_slack(channel)
+
+    # Test sending file from disk
+    file_path = "files/test.txt"
+    response = send_file(file_path, channel)
