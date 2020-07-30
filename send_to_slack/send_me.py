7e598ef29ec1bea97779562d3be0810b7e0b5374
josef.ondrej <josef.ondrej@outlook.com>
2020-07-30 11:19:53 +0200

    Add notify me

--- send_to_slack/send_me.py
+++ send_to_slack/send_me.py
@@ -1,7 +1,14 @@
-from send_to_slack.send import send_file, send_text
+import os
+
+from send_to_slack.send import send_text
+
+_notify_me_channel_env = "NOTIFY_ME_CHANNEL"
+notify_me_channel = os.environ[_notify_me_channel_env]
+
+
+def notify_me(text: str):
+    send_text(text, notify_me_channel)
+
 
 if __name__ == '__main__':
-    file_path = "C:/Temp/file"
-    channel = "@josef.ondrej"
-    # response = send_file(file_path, channel)
-    response = send_text("Hello", channel)
+    notify_me("Hey!")
